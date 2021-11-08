feaca51e7d670421f04f7026d53f6068923c47d4
Howard Tai <howard@howardism.dev>
2021-11-08 21:10:48 +0800

    feat(ui): add theme colors

---
+++ src/theme/colors.ts
@@ -0,0 +1,17 @@
+const colors = {
+  brand: {
+    0: '#6E9292',
+    1: '#A7B4AA',
+    2: '#738677',
+    3: '#E6DDB2',
+    4: '#C2BB96',
+    5: '#A7D4DA',
+  },
+  text: {
+    head: 'var(--chakra-colors-blackAlpha-800)',
+    body: 'var(--chakra-colors-blackAlpha-600)',
+    sub: 'var(--chakra-colors-blackAlpha-500)',
+  },
+};
+
+export default colors;
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,8 +1,10 @@
 import { extendTheme } from '@chakra-ui/react';
 
+import colors from './colors';
 import styles from './styles';
 
 const theme = extendTheme({
+  colors,
   styles,
   config: {
     initialColorMode: 'light',
