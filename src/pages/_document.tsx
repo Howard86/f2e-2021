68b9d2367a92fd253deff1d8eb638a9dfa1814d9
Howard Tai <howard@howardism.dev>
2021-11-08 21:30:40 +0800

    feat(ui): add custom font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,18 @@ export default class Document extends NextDocument {
   render(): JSX.Element {
     return (
       <Html>
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="''"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@400;500;700&display=optional"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
--- src/theme/styles.ts
+++ src/theme/styles.ts
@@ -2,8 +2,12 @@ import { ChakraTheme } from '@chakra-ui/react';
 
 const styles: ChakraTheme['styles'] = {
   global: {
-    'html, body, #__next': {
+    'html, #__next': {
       h: 'full',
+    },
+    body: {
+      h: 'full',
+      fontFamily: 'Noto Sans TC, sans-serif',
     },
   },
 };
