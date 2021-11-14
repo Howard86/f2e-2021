2d613a55c943a403923215e8d764cafdc2333c33
Howard Tai <howard@howardism.dev>
2021-11-14 17:41:25 +0800

    chore: remove unused api

--- src/pages/api/weather.ts
+++
@@ -1,9 +0,0 @@
-import { RouterBuilder } from 'next-api-handler';
-
-import getWeathers from '@/services/weather';
-
-const router = new RouterBuilder();
-
-router.get(() => getWeathers());
-
-export default router.build();
