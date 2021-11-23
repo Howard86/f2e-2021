6c0080eef392f13dc604f42e90b6c55e6eb17db8
Howard Tai <howard@howardism.dev>
2021-11-23 19:12:17 +0800

    feat(ui): add brand colors

--- apps/bus/src/theme/index.ts
+++ apps/bus/src/theme/index.ts
@@ -1,15 +1,18 @@
-import { extendTheme } from '@chakra-ui/react';
+import { extendTheme, withDefaultColorScheme } from '@chakra-ui/react';
 
 import colors from './colors';
 import styles from './styles';
 
-const theme = extendTheme({
-  colors,
-  styles,
-  config: {
-    initialColorMode: 'light',
-    useSystemColorMode: false,
+const theme = extendTheme(
+  {
+    colors,
+    styles,
+    config: {
+      initialColorMode: 'light',
+      useSystemColorMode: false,
+    },
   },
-});
+  withDefaultColorScheme({ colorScheme: 'primary' }),
+);
 
 export default theme;
