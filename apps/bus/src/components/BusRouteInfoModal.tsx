d3d476815d74fc26689627ddf2b61281a8541dbe
Howard Tai <howard@howardism.dev>
2021-11-28 21:51:37 +0800

    refactor(route): add BusRouteInfoModal

---
+++ apps/bus/src/components/BusRouteInfoModal.tsx
@@ -0,0 +1,100 @@
+import React from 'react';
+
+import {
+  Heading,
+  IconButton,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  ModalProps,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+} from '@chakra-ui/react';
+import { BusRouteDetail } from '@f2e/ptx';
+import { MdClose } from 'react-icons/md';
+
+import ExternalLink from './ExternalLink';
+
+interface BusRouteInfoModalProps extends Omit<ModalProps, 'children'> {
+  route: BusRouteDetail;
+}
+
+const BusRouteInfoModal = ({
+  route,
+  onClose,
+  ...props
+}: BusRouteInfoModalProps) => (
+  <Modal
+    size="full"
+    motionPreset="slideInRight"
+    colorScheme="primary"
+    scrollBehavior="inside"
+    isCentered
+    onClose={onClose}
+    {...props}
+  >
+    <ModalOverlay />
+    <ModalContent>
+      <ModalHeader textAlign="center" bg="primary.400">
+        {route.RouteName.Zh_tw}
+      </ModalHeader>
+      <IconButton
+        pos="absolute"
+        right="4"
+        top="5"
+        rounded="full"
+        size="xs"
+        color="primary.600"
+        bgColor="primary.50"
+        aria-label="close modal"
+        fontSize="xl"
+        icon={<MdClose />}
+        onClick={onClose}
+      />
+      <ModalBody textAlign="center" p="0" bg="gradient.bg">
+        <Heading p="4" fontSize="lg">
+          Start:{route.DepartureStopNameZh}-{route.DestinationStopNameZh}
+        </Heading>
+        <Tabs isFitted>
+          <TabList>
+            {route.SubRoutes.map((subRoute) => (
+              <Tab key={subRoute.SubRouteUID}>
+                {subRoute.SubRouteName.Zh_tw.replace(route.RouteName.Zh_tw, '')}
+                {subRoute.Direction}
+              </Tab>
+            ))}
+          </TabList>
+          <TabPanels>
+            {route.SubRoutes.map((subRoute) => (
+              <TabPanel key={subRoute.SubRouteUID}>
+                <Text>
+                  {subRoute.FirstBusTime}-{subRoute.LastBusTime}
+                </Text>
+                <Text>
+                  {subRoute.HolidayFirstBusTime}-{subRoute.HolidayLastBusTime}
+                </Text>
+              </TabPanel>
+            ))}
+          </TabPanels>
+          <Text>{route.TicketPriceDescriptionZh}</Text>
+          <Text>{route.FareBufferZoneDescriptionZh}</Text>
+          {route.Operators.map((operator) => (
+            <Text key={operator.OperatorID}>
+              {/* TODO: add operator details */}
+              {operator.OperatorName.Zh_tw}
+            </Text>
+          ))}
+          <ExternalLink href={route.RouteMapImageUrl}>More Info</ExternalLink>
+        </Tabs>
+      </ModalBody>
+    </ModalContent>
+  </Modal>
+);
+
+export default BusRouteInfoModal;
--- apps/bus/src/pages/city/[citySlug]/[routeName].tsx
+++ apps/bus/src/pages/city/[citySlug]/[routeName].tsx
@@ -12,11 +12,6 @@ import {
   Heading,
   HStack,
   IconButton,
-  Modal,
-  ModalBody,
-  ModalContent,
-  ModalHeader,
-  ModalOverlay,
   Tab,
   TabList,
   TabPanel,
@@ -54,7 +49,7 @@ import { GeoJSONLineString, parse } from 'wellknown';
 
 import background from '@/background-small.png';
 import bus from '@/bus.png';
-import ExternalLink from '@/components/ExternalLink';
+import BusRouteInfoModal from '@/components/BusRouteInfoModal';
 import Image from '@/components/Image';
 import { useMap } from '@/components/MapContextProvider';
 import {
@@ -466,77 +461,7 @@ const BusRoutePage = ({
           </TabPanels>
         </Tabs>
       </Flex>
-      <Modal
-        isOpen={isOpen}
-        onClose={onClose}
-        size="full"
-        motionPreset="slideInRight"
-        colorScheme="primary"
-        scrollBehavior="inside"
-        isCentered
-      >
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader textAlign="center" bg="primary.400">
-            {route.RouteName.Zh_tw}
-          </ModalHeader>
-          <IconButton
-            pos="absolute"
-            right="4"
-            top="5"
-            rounded="full"
-            size="xs"
-            color="primary.600"
-            bgColor="primary.50"
-            aria-label="close modal"
-            fontSize="xl"
-            icon={<MdClose />}
-            onClick={onClose}
-          />
-          <ModalBody textAlign="center" p="0" bg="gradient.bg">
-            <Heading p="4" fontSize="lg">
-              Start:{route.DepartureStopNameZh}-{route.DestinationStopNameZh}
-            </Heading>
-            <Tabs isFitted>
-              <TabList>
-                {route.SubRoutes.map((subRoute) => (
-                  <Tab key={subRoute.SubRouteUID}>
-                    {subRoute.SubRouteName.Zh_tw.replace(
-                      route.RouteName.Zh_tw,
-                      '',
-                    )}
-                    {subRoute.Direction}
-                  </Tab>
-                ))}
-              </TabList>
-              <TabPanels>
-                {route.SubRoutes.map((subRoute) => (
-                  <TabPanel key={subRoute.SubRouteUID}>
-                    <Text>
-                      {subRoute.FirstBusTime}-{subRoute.LastBusTime}
-                    </Text>
-                    <Text>
-                      {subRoute.HolidayFirstBusTime}-
-                      {subRoute.HolidayLastBusTime}
-                    </Text>
-                  </TabPanel>
-                ))}
-              </TabPanels>
-              <Text>{route.TicketPriceDescriptionZh}</Text>
-              <Text>{route.FareBufferZoneDescriptionZh}</Text>
-              {route.Operators.map((operator) => (
-                <Text key={operator.OperatorID}>
-                  {/* TODO: add operator details */}
-                  {operator.OperatorName.Zh_tw}
-                </Text>
-              ))}
-              <ExternalLink href={route.RouteMapImageUrl}>
-                More Info
-              </ExternalLink>
-            </Tabs>
-          </ModalBody>
-        </ModalContent>
-      </Modal>
+      <BusRouteInfoModal isOpen={isOpen} onClose={onClose} route={route} />
       {selectedStop && (
         <Drawer
           isOpen={stopDisclosure.isOpen}
